2f922a86dfff81c4a4d00c54cb1d429e9baa5270
An Pham <ptan1991@gmail.com>
2018-10-31 13:45:27 +0100

    - node storing and searching strategy

--- src/main/java/PeerNode.java
+++ src/main/java/PeerNode.java
@@ -9,7 +9,11 @@ import cluster.PeerBroker;
 import org.zeromq.ZContext;
 import org.zeromq.ZMQ;
 
+import java.lang.reflect.Array;
+import java.util.BitSet;
+import java.util.Hashtable;
 import java.util.Random;
+import smile.math.distance.HammingDistance;
 
 public class PeerNode {
     private PeerBroker peerCluster;
@@ -19,7 +23,7 @@ public class PeerNode {
     private int chunkBytes;
     private float hammingT;
 
-    private String[][] storageArr;
+    private Object[][] storageArr;
 
 
     public PeerNode(PeerBroker cluster, int nChunks, int chSize, int thresholdT) {
@@ -86,54 +90,88 @@ public class PeerNode {
         insertbe.connect("tcp://localhost:2262");
     }
 
-    private int hammingDist(String left, String right) {
-        if (left.length() != right.length() || left.getBytes().length != chunkBytes)
-            return -1;
-
-        int distance = 0;
-
-        for (int i = 0; i < left.length(); i++) {
-            if (left.charAt(i) != right.charAt(i)) {
-                distance++;
-            }
-        }
-
-        return distance;
-    }
+//    private int hammingDist(BitSet left, BitSet right) {
+//        if (left.size() != right.size()) return -1;
+//
+//        HammingDistance.d(left, right);
+//
+//        int distance = 0;
+//
+//        for (int i = 0; i < left.size(); i++) {
+//            if (left.charAt(i) != right.charAt(i)) {
+//                distance++;
+//            }
+//        }
+//
+//        return distance;
+//    }
 
     private void initializeStorage() {
-        storageArr = new String[maxChunks][];
-        for (int i = 0; i < storageArr.length; i++) {
-            String[] arr = new String[2];
-            byte[] rand = new byte[chunkBytes];
-            new Random().nextBytes(rand);
-            String key = new String(rand);
+        storageArr = new Object[maxChunks][];
+        for (int i = 0; i < maxChunks; i++) {
+            Object[] arr = new Object[2];
 
-            arr[0] = key;
-            arr[1] = new String(new byte[chunkBytes]);
-
+            arr[0] = randomize();
+            arr[1] = new int[chunkBits];
             storageArr[i] = arr;
         }
     }
 
-    private int insert(String inputStr) {
+//    private int[] bipolarEncode(BitSet input) {
+//        int[] arr = new int[chunkBits];
+//        for (int i=0; i<chunkBits; i++) {
+//            int val = input.get(i) ? 1 : -1;
+//            arr[i] = val;
+//        }
+//        return arr;
+//    }
+
+    private int[] sumOf(int[] curr, BitSet input) {
+//        int[] arr = new int[chunkBits];
+        if (input.size() != chunkBits) {
+            System.out.println("Invalid format of input, should be " + chunkBits + " bits in size");
+            return new int[chunkBits];
+        }
+        for (int i=0; i<chunkBits; i++) {
+            int val = input.get(i) ? 1 : -1;
+            curr[i] += val;
+        }
+        return curr;
+    }
+
+    private BitSet randomize() {
+        Random random = new Random();
+        BitSet bits = new BitSet(chunkBits);
+        for (int i = 0; i < chunkBits; ++i)
+            bits.set(i, random.nextBoolean());
+
+        return bits;
+    }
+
+    private int insert(BitSet inputStr) {
         // Check size constraint
-        if (inputStr.getBytes().length != chunkBytes) return 0;
+        if (inputStr.size() != chunkBits) return 0;
 
         // Find suitable memory
         for (int i = 0; i < storageArr.length; i++) {
-            int dist = hammingDist(storageArr[i][0], inputStr);
-            if (dist <= hammingT) {
-                storageArr[i][1] = inputStr;
+            int addrDist = HammingDistance.d((BitSet)storageArr[i][0], inputStr);
+            if (addrDist <= hammingT) {
+                storageArr[i][1] = sumOf((int[])storageArr[i][1], inputStr);
             }
         }
-        // Save
         return 1;
     }
 
-    private int search(String inputStr) {
+    private int[] match(BitSet inputStr) {
         // Check size constraint
-        if (inputStr.getBytes().length != chunkBytes) return 0;
-        return 0;
+        if (inputStr.size() != chunkBits) return null;
+        int[] resultArr = new int[chunkBits];
+        for (int i = 0; i < storageArr.length; i++) {
+            int addrDist = HammingDistance.d((BitSet) storageArr[i][0], inputStr);
+            if (addrDist <= hammingT) {
+                resultArr = sumOf(resultArr, inputStr);
+            }
+        }
+        return resultArr;
     }
 }
