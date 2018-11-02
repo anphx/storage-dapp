f1d0c6774ab341e5f65c3482fbcdc135a7bdfce8
AmiR <rahafrouz@gmail.com>
2018-11-02 10:58:12 +0100

    clusterconfig file updated

--- clusterconfig.sh
+++ clusterconfig.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 ulimit -n 100000
-wget https://s3-us-west-2.amazonaws.com/miniprojectcluster/cluster.jar
-java -jar cluster.jar 100 1000 64 20 25 0 1
+curl -0 https://github.com/an-pham/storage-dapp/raw/master/out/artifacts/cluster/cluster.jar --output cluster.jar
+#wget https://s3-us-west-2.amazonaws.com/miniprojectcluster/cluster.jar
+java -jar cluster.jar 100 100 64 20 25 0 1
 
 
