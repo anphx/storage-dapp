04600183be64ef8d0b5a051f4d80679f50ace622
AmiR <rahafrouz@gmail.com>
2018-11-02 11:58:06 +0100

    correct cluster file address in the github

--- clusterconfig.sh
+++ clusterconfig.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ulimit -n 100000
-curl -0 https://github.com/an-pham/storage-dapp/raw/master/out/artifacts/cluster/cluster.jar --output cluster.jar
+curl -0 https://raw.githubusercontent.com/an-pham/storage-dapp/master/out/artifacts/cluster/cluster.jar --output cluster.jar
 #wget https://s3-us-west-2.amazonaws.com/miniprojectcluster/cluster.jar
 java -jar cluster.jar 100 100 64 20 25 0 1
 
